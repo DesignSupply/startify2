cc7f0d55957c92057a1968cbb0e10b81e5568f4c
DesignSupply <ogawa@designsupply-web.com>
2021-05-09 06:07:26 +0900

    [add] ソースファイル一式追加

---
+++ develop/src/es/main.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// stylesheet import
+import '../sass/main.sass';
+// import '../scss/main.scss';
+
+// const $ = require('jQuery');
+// const Vue = require('vue');
+
+// checking legacy IE
+const isLegacyIE = () => {
+  const currentUserAgent = window.navigator.userAgent.toLowerCase(),
+    notice = 'ご利用のウェブブラウザでは当サイトを正常に閲覧できません。ブラウザのバージョンを最新にして再度アクセスしてください。',
+    targets = [ 
+      'msie 6.0', // Internet Explorer 6
+      'msie 7.0', // Internet Explorer 7
+      'msie 8.0', // Internet Explorer 8
+      'msie 9.0', // Internet Explorer 9
+      'msie 10.0', // Internet Explorer 10
+      'trident/7.0' // Internet Explorer 11
+    ];
+    Array.prototype.slice.call(targets).forEach((target) => {
+    if(currentUserAgent.indexOf(target) !== -1) {
+      alert(notice);
+    } else {
+      return false;
+    }
+  });
+};
+isLegacyIE();
+
+// checking OS
+const findOS = () => {
+  const currentUserAgent = window.navigator.userAgent.toLowerCase();
+  if(currentUserAgent.indexOf('windows nt') !== -1) {
+    return 'Windows';
+  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') !== -1)) {
+    return 'Android Mobile';
+  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') === -1)) {
+    return 'Android Tablet';
+  } else if(currentUserAgent.indexOf('iphone') !== -1) {
+    return 'iOS iPhone';
+  } else if(currentUserAgent.indexOf('ipad') !== -1) {
+    return 'iOS iPad';
+  } else if(currentUserAgent.indexOf('mac os') !== -1) {
+    return 'Mac OS';
+  } else {
+    return 'other OS';
+  }
+};
+findOS();
+
+// checking browser
+const findBrowser = () => {
+  const currentUserAgent = window.navigator.userAgent.toLowerCase();
+  if(currentUserAgent.indexOf('msie') !== -1 || currentUserAgent.indexOf('trident') !== -1) {
+    return 'Internet Explorer';
+  } else if(currentUserAgent.indexOf('edge') !== -1) {
+    return 'Microsoft Edge';
+  } else if(currentUserAgent.indexOf('edg') !== -1) {
+    return 'Chromium Edge';
+  } else if(currentUserAgent.indexOf('opr') !== -1) {
+    return 'Opera';
+  } else if(currentUserAgent.indexOf('chrome') !== -1) {
+    return 'Google Chrome';
+  } else if(currentUserAgent.indexOf('safari') !== -1) {
+    return 'Safari';
+  } else if(currentUserAgent.indexOf('firefox') !== -1) {
+    return 'FireFox';
+  } else {
+    return 'other Browser';
+  }
+};
+findBrowser();
+
+// jQuery
+// $(function() {
+//   console.log('jQuery is ready.');
+// });
+
+// Vue.js
+// const vueModel = new Vue({
+//   el: '#app',
+//   data() {
+//     return {
+//       message: 'Vue.js is ready.'
+//     }
+//   },
+//   created() {
+//     this.showMessage();
+//   },
+//   methods: {
+//     showMessage() {
+//       console.log(this.message);
+//     }
+//   }
+// });
---
+++ develop/webpack.config.js
@@ -0,0 +1,137 @@
+// modules include
+const path = require('path');
+const globule = require('globule');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+// build settings
+const buildMode = 'development'; // production or development
+const cssInline = false;
+const useTypeScript = true;
+const directoryPath = {
+  root: path.resolve(__dirname, ''),
+  dist: path.resolve(__dirname, 'dist'),
+  src: path.resolve(__dirname, 'src')
+}
+
+// build configuration
+let styleLoader = null;
+let entryPointPath = null;
+const pugFiles = globule.find('src/pug/**/*.pug', {
+  ignore: [
+    'src/pug/**/_*.pug'
+  ]
+});
+if(cssInline) {
+  styleLoader = 'style-loader';
+} else {
+  styleLoader = { loader: MiniCssExtractPlugin.loader };
+}
+if(useTypeScript) {
+  entryPointPath = `${directoryPath.src}/ts/main.ts`;
+} else {
+  entryPointPath = `${directoryPath.src}/es/main.js`;
+}
+const buildDefault = {
+  mode: buildMode,
+  devtool: 'source-map',
+  entry: entryPointPath,
+  output: {
+    path: `${directoryPath.dist}/assets`,
+    filename: 'js/main.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [ '@babel/preset-env' ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          styleLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [ 'autoprefixer', { grid: true } ],
+                  [ 'cssnano', { preset: 'default' } ]
+                ]
+              },
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
+        test: /\.pug$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'pug-loader',
+            options: {
+              pretty: true,
+              root: `${directoryPath.src}/pug`
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'css/main.min.css' }),
+    new StylelintPlugin({ configFile: `${directoryPath.root}/.stylelintrc` }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: directoryPath.dist }
+    })
+  ],
+  target: [ 'web', 'es5' ]
+}
+
+// page template output
+pugFiles.forEach((pug) => {
+  const html = pug.split('/').slice(-1)[0].replace('.pug', '.html');
+  buildDefault.plugins.push(
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: `${directoryPath.dist}/${html}`,
+      template: pug,
+      data: require(`${directoryPath.src}/pug/data/global.js`)
+    })
+  )
+});
+
+module.exports = buildDefault;
