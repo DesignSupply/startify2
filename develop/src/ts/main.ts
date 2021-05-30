38cbcfef3c7bebf46eeab0c1cd2f621f73e3bf0c
DesignSupply <ogawa@designsupply-web.com>
2021-05-31 01:35:36 +0900

    [add] 共通処理をモジュール化（TypeScript）

--- develop/src/ts/main.ts
+++ develop/src/ts/main.ts
@@ -6,8 +6,10 @@ import '../sass/main.sass';
 
 import $ from 'jquery';
 import Vue from 'vue';
-import moduleFunction from './modules/module';
 import { IntersectionObserverToggleClass } from './modules/class-intersection-observer-toggleclass';
+import { IsLegacyIE } from './modules/checking-legacy-ie';
+import { FindOS } from './modules/checking-os';
+import { FindBrowser } from './modules/checking-browser';
 import App from './vue/app.vue';
 
 // Vue.js
@@ -25,70 +27,11 @@ new IntersectionObserverToggleClass({
   toggle: true
 });
 
-console.log(moduleFunction('Hello World! use TypeScript'));
-
 // checking legacy IE
-const isLegacyIE = (): void | boolean => {
-  const currentUserAgent: string = window.navigator.userAgent.toLowerCase(),
-    notice = 'ご利用のウェブブラウザでは当サイトを正常に閲覧できません。ブラウザのバージョンを最新にして再度アクセスしてください。',
-    targets: Array<string> = [ 
-      'msie 6.0', // Internet Explorer 6
-      'msie 7.0', // Internet Explorer 7
-      'msie 8.0', // Internet Explorer 8
-      'msie 9.0', // Internet Explorer 9
-      'msie 10.0', // Internet Explorer 10
-      'trident/7.0' // Internet Explorer 11
-    ];
-    Array.prototype.slice.call(targets).forEach((target: string): void | boolean => {
-    if(currentUserAgent.indexOf(target) !== -1) {
-      alert(notice);
-    } else {
-      return false;
-    }
-  });
-};
-isLegacyIE();
+IsLegacyIE();
 
 // checking OS
-const findOS = (): string => {
-  const currentUserAgent: string = window.navigator.userAgent.toLowerCase();
-  if(currentUserAgent.indexOf('windows nt') !== -1) {
-    return 'Windows';
-  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') !== -1)) {
-    return 'Android Mobile';
-  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') === -1)) {
-    return 'Android Tablet';
-  } else if(currentUserAgent.indexOf('iphone') !== -1) {
-    return 'iOS iPhone';
-  } else if(currentUserAgent.indexOf('ipad') !== -1) {
-    return 'iOS iPad';
-  } else if(currentUserAgent.indexOf('mac os') !== -1) {
-    return 'Mac OS';
-  } else {
-    return 'other OS';
-  }
-};
-findOS();
+FindOS();
 
 // checking browser
-const findBrowser = (): string => {
-  const currentUserAgent: string = window.navigator.userAgent.toLowerCase();
-  if(currentUserAgent.indexOf('msie') !== -1 || currentUserAgent.indexOf('trident') !== -1) {
-    return 'Internet Explorer';
-  } else if(currentUserAgent.indexOf('edge') !== -1) {
-    return 'Microsoft Edge';
-  } else if(currentUserAgent.indexOf('edg') !== -1) {
-    return 'Chromium Edge';
-  } else if(currentUserAgent.indexOf('opr') !== -1) {
-    return 'Opera';
-  } else if(currentUserAgent.indexOf('chrome') !== -1) {
-    return 'Google Chrome';
-  } else if(currentUserAgent.indexOf('safari') !== -1) {
-    return 'Safari';
-  } else if(currentUserAgent.indexOf('firefox') !== -1) {
-    return 'FireFox';
-  } else {
-    return 'other Browser';
-  }
-};
-findBrowser();
+FindBrowser();
