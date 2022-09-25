b39f5f388f1b6cf782b63b285ab566530a8e9b24
DesignSupply <ogawa@designsupply-web.com>
2022-09-25 13:22:14 +0900

    [change] setup構文に対応（TypeScript）

--- src/ts/vue/app.vue
+++ src/ts/vue/app.vue
@@ -2,34 +2,24 @@
   <h1>Startify 2</h1>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
   import { defineComponent, reactive, onMounted, computed } from 'vue';
   import { useStore } from 'vuex';
   import { key } from '../store';
   interface dataInterface {
     message: string
   }
-  export default defineComponent({
-    setup() {
-      const store = useStore(key);
-      const data:dataInterface = reactive({ message: 'Vue.js is ready use TypeScript' });
-      store.dispatch('updateStatus', 'state updated');
-      const stateStatus = computed(() => {
-        return store.getters.getStatus;
-      })
-      const showMessage = ():void => {
-        console.log(data.message);
-        console.log(stateStatus.value);
-      };
-      onMounted(():void => {
-        showMessage();
-      });
-      return {
-        store,
-        data,
-        stateStatus,
-        showMessage
-      }
-    }
+  const store = useStore(key);
+  const data:dataInterface = reactive({ message: 'Vue.js is ready use TypeScript' });
+  store.dispatch('updateStatus', 'state updated');
+  const stateStatus = computed(() => {
+    return store.getters.getStatus;
+  })
+  const showMessage = ():void => {
+    console.log(data.message);
+    console.log(stateStatus.value);
+  };
+  onMounted(():void => {
+    showMessage();
   });
 </script>
